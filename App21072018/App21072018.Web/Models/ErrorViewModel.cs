33f0c018df9d19f3ecef9ba21542d93acb7d98f1
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-07-21 23:56:18 +0700

    add web

---
+++ App21072018/App21072018.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace App21072018.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
