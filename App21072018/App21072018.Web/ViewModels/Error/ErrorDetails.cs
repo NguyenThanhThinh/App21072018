7508ed9568f725d8a79999310462870f26eb85a6
Thinh Nguyen <thanhthinhcntt@gmail.com>
2018-08-18 14:59:02 +0700

    add errordetail

---
+++ App21072018/App21072018.Web/ViewModels/Error/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace App21072018.Web.ViewModels.Error
+{
+    public class ErrorDetails
+    {
+        public string StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
